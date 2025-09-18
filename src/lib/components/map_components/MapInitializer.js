5bd81a1ca3c58bcd228959f31ce84f6851f8c8d5
John Lloyd Umani <umani.johnlloyd.esplana@gmail.com>
2025-09-18 13:42:56 +0800

    Change minZoom from 19 to 1

--- src/lib/components/map_components/MapInitializer.js
+++ src/lib/components/map_components/MapInitializer.js
@@ -20,7 +20,7 @@ export async function initializeMap(L, mapContainer) {
     center: paddedNcrBounds.getCenter(),
     maxBounds: paddedNcrBounds,
     zoom: 10,
-    minZoom: 19,
+    minZoom: 1,
     maxBoundsViscosity: 0.9
   });
 // zoom: 11,
