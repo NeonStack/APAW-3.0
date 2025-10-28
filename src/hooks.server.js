6ca356e598722a2d6ee6dd55c019ddc2619143a7
NeonStack <umani.johnlloyd.esplana@gmail.com>
2025-10-28 14:15:07 +0800

    refactor: Update allowed origins and parameters for flood prediction API

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,14 +4,14 @@ import { json, error } from '@sveltejs/kit';
 import { building } from '$app/environment';
 import { paramChecker } from '$lib/utils/api/paramChecker';
 
-const ALLOWED_ORIGIN = ['https://apawph.vercel.app', 'http://localhost:5173'];
+const ALLOWED_ORIGIN = ['https://apawph.vercel.app', 'http://localhost:5173', 'https://apawph-development.vercel.app/'];
 const EXCEPTIONS = ['/api/update-weather'];
 const CACHED_API_ROUTES = ['/api/get-weather', '/api/water-stations'];
 const API_PARAM_CONFIG = {
 	'/api/get-weather': ['location'],
 	'/api/water-stations': [],
 	'/api/elevation': ['lat', 'lng'],
-	'/api/flood-prediction': ['lat', 'lng', 'date'],
+	'/api/flood-prediction': ['lat', 'lng', 'date', 'elevation', 'water_station_data'],
 	'/api/tropicalCyclone-tracker': []
 };
 
